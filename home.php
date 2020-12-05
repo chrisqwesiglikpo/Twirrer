38063c8fa6fdf193bea99619db7e8a9e40f06e27
glikpochristopher <chris.glikpo@netwareitsolutions.com>
2020-12-05 08:51:46 -0500

    Completed Suggestion featured

--- home.php
+++ home.php
@@ -70,7 +70,49 @@ $userId=$user->user_id;
                    <svg viewBox="0 0 24 24" class="search-icon"><g><path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path></g></svg>
                </form>
                <div id="search-show">
-                    <div class="search-result"></div>
+                    <div class="search-result">
+                        <div class="search-title">
+                            <div class="search-header">
+                                <h3>Try searching for people, topics, or keywords</h3>
+                            </div>
+                        </div>
+                        <ul id="suggestion">
+                            <li>
+                                <a href="#">
+                                    <div id="image-wrapper-suggest">
+                                        <img src="<?php echo url_for('frontend/assets/images/profilePic.jpeg') ?>" alt="User Profile Pic">
+                                    </div>
+                                    <div class="suggest-name">
+                                        <h2>Christopher Glikpo</h2>
+                                        <h4>@cglikpo</h4>
+                                    </div>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <div id="image-wrapper-suggest">
+                                        <img src="<?php echo url_for('frontend/assets/images/defaultProfilePic.png') ?>" alt="User Profile Pic">
+                                    </div>
+                                    <div class="suggest-name">
+                                        <h2>Daniel Attoh</h2>
+                                        <h4>@danielattoh</h4>
+                                    </div>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <div id="image-wrapper-suggest">
+                                        <img src="<?php echo url_for('frontend/assets/images/profilePic.jpeg') ?>" alt="User Profile Pic">
+                                    </div>
+                                    <div class="suggest-name">
+                                        <h2>Micheal Otu</h2>
+                                        <h4>@michael</h4>
+                                    </div>
+                                </a>
+                            </li>
+
+                        </ul>
+                    </div>
                </div>
             </div>
             <div class="follow">
