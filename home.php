bfbcc8ec86feb1cabc6c87c33e283c6a295153b7
glikpochristopher <chris.glikpo@netwareitsolutions.com>
2020-12-05 07:06:09 -0500

    Completed the search-area

--- home.php
+++ home.php
@@ -32,8 +32,8 @@ $userId=$user->user_id;
                    <div class="userImageContainer">
                       <img src="<?php echo $user->profilePic; ?>" alt="User Profile Pic">
                    </div>
-                   <form class="textareaContainer" > 
-                        <textarea  id="postTextarea" placeholder="What's happening?"></textarea>
+                   <form class="textareaContainer"> 
+                        <textarea  id="postTextarea" placeholder="What's happening?" aria-label="What's happening?"></textarea>
                         <div class="hash-box">
                            <ul></ul>
                         </div>
@@ -64,12 +64,14 @@ $userId=$user->user_id;
                 
         </section>
         <aside role="complementary">
-            <div class="search-wrap">
-                <input type="text" name="main-search" id="main-search" placeholder="Search Twitter"/>
-                <svg viewBox="0 0 24 24" class="search-icon"><g><path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path></g></svg>
-            </div>
-            <div id="search-show">
-              <div class="search-result"></div>
+            <div id="search-area">
+               <form id="search-form" aria-label="Search Twitter" role="search">
+                   <input type="text" name="main-search" id="main-search" placeholder="Search Twitter" role="searchbox"/>
+                   <svg viewBox="0 0 24 24" class="search-icon"><g><path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path></g></svg>
+               </form>
+               <div id="search-show">
+                    <div class="search-result"></div>
+               </div>
             </div>
             <div class="follow">
             <h3 class="follow-heading">Who to follow</h3>
