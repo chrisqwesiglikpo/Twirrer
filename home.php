3b02c8c61abb57e8e30906b346dcc8dac7882141
glikpochristopher <chris.glikpo@netwareitsolutions.com>
2020-12-05 05:00:27 -0500

    Modifies the aside component

--- home.php
+++ home.php
@@ -63,7 +63,15 @@ $userId=$user->user_id;
                 </div> 
                 
         </section>
-        <aside role="complementary" class="follow">
+        <aside role="complementary">
+            <div class="search-wrap">
+                <input type="text" name="main-search" id="main-search" placeholder="Search Twitter"/>
+                <svg viewBox="0 0 24 24" class="search-icon"><g><path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path></g></svg>
+            </div>
+            <div id="search-show">
+              <div class="search-result"></div>
+            </div>
+            <div class="follow">
             <h3 class="follow-heading">Who to follow</h3>
                <?php $loadFromFollow->whoToFollow($user_id,$profileId); ?>
             <div class="follow-link">
@@ -78,6 +86,8 @@ $userId=$user->user_id;
                     <li><a href="#">More</a></li>
                 </ul>
             </footer>
+            
+            </div>
         </aside>
         </main>
 </section>
