ebd63d6e10749bda92f9f25ff93191ee4e2f3a71
glikpochristopher <chris.glikpo@netwareitsolutions.com>
2020-12-09 12:19:53 -0500

    Fix bug ar profileContainer

--- frontend/assets/js/profile.js
+++ frontend/assets/js/profile.js
@@ -1,15 +1,31 @@
-$(document).ready(function(){
-    loadPosts();
+var u_id = $('.u_p_id').data('uid');
+$(function(){
+
+    $(document).ready(function(){
+        loadPosts();
+    })
+    
+    function loadPosts(){
+        // var p_id = $('.u_p_id').data('pid');
+        var u_id = $('.u_p_id').data('uid');
+        var offset=10;
+                // $('#loader').show();
+                $.post('http://localhost/twirrer/backend/ajax/fetchPosts.php',
+                    {fetchPosts:offset,userid:u_id},function(data){
+                     $('.postsContainer').html(data);
+                    //  $('#loader').hide();
+            });
+    }
+
+    $(document).on("change","#cover-upload",function(){
+        let name=$("#cover-upload").val().split("\\").pop();
+        let fileData=$("#cover-upload").prop("files")[0];
+        let fileSize=fileData['size'];
+        let fileType=fileData['type'].split('/').pop();
+
+        let userId=u_id;
+        alert(fileSize);
+    })
+
 })
 
-function loadPosts(){
-    // var p_id = $('.u_p_id').data('pid');
-    var u_id = $('.u_p_id').data('uid');
-    var offset=10;
-            // $('#loader').show();
-            $.post('http://localhost/twirrer/backend/ajax/fetchPosts.php',
-                {fetchPosts:offset,userid:u_id},function(data){
-                 $('.postsContainer').html(data);
-                //  $('#loader').hide();
-        });
-}
