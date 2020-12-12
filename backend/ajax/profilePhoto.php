d917b8608a70efb500e5cfb6fac4b2131496ac87
glikpochristopher <chris.glikpo@netwareitsolutions.com>
2020-12-12 03:53:33 -0500

    Removed duplicate code

--- backend/ajax/profilePhoto.php
+++ backend/ajax/profilePhoto.php
@@ -1,23 +1,14 @@
 <?php
  require_once('../init.php');
 
+
  if($_SERVER['REQUEST_METHOD'] === "POST"){
     // var_dump($_FILES['croppedImage']);
      if(!empty($_FILES['croppedImage'])){
       $userid=$_POST['userId'];
-      $path_directory = $_SERVER['DOCUMENT_ROOT']."/Twirrer/frontend/profileImage/".$userid.'/';
-
-        if(!file_exists($path_directory) && !is_dir($path_directory)){
-            mkdir($path_directory, 0777, true);
-        
-        }
-       $folder=$path_directory.$_FILES['croppedImage']['name'].'.png';
-     
-        move_uploaded_file($_FILES['croppedImage']['tmp_name'],$folder);
-        $image_path="frontend/profileImage/".$userid.'/'.$_FILES['croppedImage']['name'].'.png';
-        $loadFromUser->update("users",$userid,array('profilePic'=>$image_path));
-        echo "frontend/profileImage/".$userid.'/'.$_FILES['croppedImage']['name'].'.png';
-
+      $imagePath=$loadFromUser->cropImageUpload($_FILES['croppedImage'],$userid);
+      $loadFromUser->update("users",$userid,array('profilePic'=>$imagePath));
+      
      }
       
  }  
