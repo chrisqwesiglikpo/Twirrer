3aafef71bedde31015078047a91023fefb24204c
glikpochristopher <chris.glikpo@netwareitsolutions.com>
2020-12-19 05:27:26 -0500

    Completed creating the postImageUpload at User class

--- backend/classes/User.php
+++ backend/classes/User.php
@@ -210,7 +210,41 @@ class User{
             }
         }
     }
- 
+   
+    public function uploadPostImage($file,$user_id){
+        $fileInfo     = getimagesize($file['tmp_name']);
+        $fileTmp      = $file['tmp_name'];
+        $fileName     = $file['name'];
+        $fileSize     = $file['size'];
+        $errors       = $file['error'];
+
+        //get extenion
+        $ext   = explode('.', $fileName);
+        $ext   = strtolower(end($ext));
+
+        //extenions types 
+        $allowed  = array('image/png', 'image/jpeg', 'image/jpg');
+
+        if(in_array($fileInfo['mime'], $allowed)){
+            // $folder = 'frontend/content/'.$user_id.'/profilePic/';
+           
+            $path_directory = $_SERVER['DOCUMENT_ROOT']."/Twirrer/frontend/media/";
+
+            if(!file_exists($path_directory) && !is_dir($path_directory)){
+                mkdir($path_directory, 0777, true);
+            
+            }
+         
+            $folder   ="frontend/media/".substr(md5(time().mt_rand()), 2,25).'.'.$ext;
+            $file=$_SERVER['DOCUMENT_ROOT']."/Twirrer/".$folder;
+            if($errors === 0){
+            
+                move_uploaded_file($fileTmp,$file);
+                return $folder;
+                
+            }
+        }
+    }
      
 
     public function timeAgo($datetime){
